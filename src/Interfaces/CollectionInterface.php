615d5906f33262cc347440f3649c2b0c1efc5c65
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-05-07 16:01:59 +0200

    feat(interfaces): create CollectionInterface.php
    The Collection interface represents a collection of items and is the base interface for all interfaces in the Collections namespace.

---
+++ src/Interfaces/CollectionInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Assegai\Collections\Interfaces;
+
+use Countable;
+use IteratorAggregate;
+use JsonSerializable;
+use Serializable;
+use Stringable;
+
+/**
+ * Represents a collection of items.
+ *
+ * @template T
+ */
+interface CollectionInterface extends Countable, IteratorAggregate, JsonSerializable, Serializable, Stringable, ComparableInterface
+{
+  /**
+   * Copies the items in the collection to an array.
+   *
+   * @template T
+   * @return array<T> The items in the collection.
+   */
+  public function toArray(): array;
+
+  /**
+   * Removes all items from the collection.
+   *
+   * @return void
+   */
+  public function clear(): void;
+
+  /**
+   * Determines whether the collection contains a specific item.
+   *
+   * @template T
+   * @param T $other The item to locate in the collection.
+   * @return bool True if the item is found; otherwise, false.
+   */
+  public function contains(mixed $other): bool;
+}
