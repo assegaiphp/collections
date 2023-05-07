bce8cb117d96571a4b992a45ca0f36cc0e6ddd61
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-05-07 15:53:06 +0200

    feat(interfaces): create ComparableInterface.php
    The ComparableInterface represents an object that can be compared to another.

---
+++ src/Interfaces/ComparableInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Assegai\Collections\Interfaces;
+
+/**
+ * Represents an object that can be compared for equality with another object.
+ *
+ * @template T
+ */
+interface ComparableInterface
+{
+  /**
+   * Compares the current object with another object of the same type.
+   *
+   * @template T
+   * @param T $other The object to compare with the current object.
+   * @return int A value that indicates the relative order of the objects being compared. The return value has these meanings:
+   *             - less than zero: The current object is less than the other parameter.
+   *             - zero: The current object is equal to the other.
+   *             - greater than zero: The current object is greater than the other.
+   */
+  public function compareTo(mixed $other): int;
+}
