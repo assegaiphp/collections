8ffca5c23d6dfec7f5d6210edd6963ae9e6fce0e
Andrew Masiye <amasiye313@gmail.com>
2023-05-08 02:56:23 +0200

    feat: implement remove method in Collection.php

--- src/Collection.php
+++ src/Collection.php
@@ -14,14 +14,14 @@ class Collection extends AbstractCollection
   /**
    * Adds an item to the collection.
    * @template T
-   * @param T $other The item to add.
+   * @param T $item The item to add.
    * @return void
    */
-  public function add(mixed $other): void
+  public function add(mixed $item): void
   {
     $typeName = match (true) {
-      is_object($other) => get_class($other),
-      default => gettype($other),
+      is_object($item) => get_class($item),
+      default => gettype($item),
     };
 
     if ($typeName !== $this->type)
@@ -29,6 +29,17 @@ class Collection extends AbstractCollection
       throw new TypeError( $this->getTypeErrorMessage(__METHOD__, $typeName) );
     }
 
-    $this->items[] = $other;
+    $this->items[] = $item;
+  }
+
+  /**
+   * Removes an item from the collection.
+   * @template T The type of the item.
+   * @param T $item The item to remove.
+   * @return void
+   */
+  public function remove(mixed $item): void
+  {
+    $this->items = array_filter($this->items, fn($i) => $i !== $item);
   }
 }
