fac7b10c50a73dc4d2c70cf7d5bb9ebed8fb13b4
Andrew Masiye <amasiye313@gmail.com>
2025-03-09 00:02:00 +0200

    refactor: reformat Set.php and ItemList.php

--- tests/Unit/SetTest.php
+++ tests/Unit/SetTest.php
@@ -2,37 +2,39 @@
 
 use Assegai\Collections\Set;
 
-it('can add items to the collection using the constructor',  function () {
-  $set = new Set('string', ['foo', 'bar', 'baz']);
+describe('Set', function() {
+  it('can add items to the set using the constructor',  function () {
+    $set = new Set('string', ['foo', 'bar', 'baz']);
 
-  expect($set->count())->toBe(3)
-    ->and($set->toArray())->toBe(['foo', 'bar', 'baz']);
-});
+    expect($set->count())->toBe(3)
+      ->and($set->toArray())->toBe(['foo', 'bar', 'baz']);
+  });
 
-it('can add items to the collection',  function () {
-  $set = new Set('string');
-  $set->add('foo');
-  $set->add('bar');
-  $set->add('baz');
+  it('can add items to the set',  function () {
+    $set = new Set('string');
+    $set->add('foo');
+    $set->add('bar');
+    $set->add('baz');
 
-  expect($set->count())->toBe(3)
-    ->and($set->toArray())->toBe(['foo', 'bar', 'baz']);
+    expect($set->count())->toBe(3)
+      ->and($set->toArray())->toBe(['foo', 'bar', 'baz']);
 
-  $set->addAll(['foo1', 'bar1', 'baz1']);
+    $set->addAll(['foo1', 'bar1', 'baz1']);
 
-  expect($set->count())->toBe(6)
-    ->and($set->toArray())->toBe(['foo', 'bar', 'baz', 'foo1', 'bar1', 'baz1']);
+    expect($set->count())->toBe(6)
+      ->and($set->toArray())->toBe(['foo', 'bar', 'baz', 'foo1', 'bar1', 'baz1']);
 
-  $set->addAll(['foo', 'bar', 'baz']);
+    $set->addAll(['foo', 'bar', 'baz']);
 
-  expect($set->count())->toBe(6)
-    ->and($set->toArray())->toBe(['foo', 'bar', 'baz', 'foo1', 'bar1', 'baz1']);
-});
+    expect($set->count())->toBe(6)
+      ->and($set->toArray())->toBe(['foo', 'bar', 'baz', 'foo1', 'bar1', 'baz1']);
+  });
 
-it('can remove items from the collection',  function () {
-  $set = new Set('string', ['foo', 'bar', 'baz']);
-  $set->remove('bar');
+  it('can remove items from the set',  function () {
+    $set = new Set('string', ['foo', 'bar', 'baz']);
+    $set->remove('bar');
 
-  expect($set->count())->toBe(3)
-    ->and($set->toArray())->toBe(['foo', 'baz']);
+    expect($set->count())->toBe(2)
+      ->and($set->toArray())->toBe([0 => 'foo', 2 => 'baz']);
+  });
 });
